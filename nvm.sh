c032d7537af252fd4f5037f19084860486c8ab0f
Bilal Bhatti <bbhatti@gmail.com>
2018-06-14 02:03:26 -0500

    cleanup

--- nvm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Setting up NVM..."
-
-# Install nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
-
-nvm install node
-
--- setup.sh
+++ setup.sh
@@ -13,3 +13,17 @@ brew update
 # Install all our dependencies with bundle (See Brewfile)
 brew bundle
 
+echo "Setting up your Mac..."
+
+# Remove junk files
+find ./ -name ".DS_Store" -depth -exec rm {} \;
+
+stow home --no-folding
+
+
+echo "Setting up NVM..."
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+nvm install node
